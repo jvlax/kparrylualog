b4494a940a71b755251a8c51e0b35ecb60ae431d
Johannes Valter-Lax <42671808+jvlax@users.noreply.github.com>
2022-12-06 11:27:54 +0100

    Update log.lua
    forgot to add this function

--- log.lua
+++ log.lua
@@ -38,6 +38,31 @@ end
 
 -- print table as string, just used for debug purpose
 
+function table_to_string(tbl)
+    local result = "{"
+    for k, v in pairs(tbl) do
+        -- Check the key type (ignore any numerical keys - assume its an array)
+        if type(k) == "string" then
+            result = result.."[\""..k.."\"]".."="
+        end
+
+        -- Check the value type
+        if type(v) == "table" then
+            result = result..table_to_string(v)
+        elseif type(v) == "boolean" then
+            result = result..tostring(v)
+        else
+            result = result.."\""..v.."\""
+        end
+        result = result..","
+    end
+    -- Remove leading commas from the result
+    if result ~= "" then
+        result = result:sub(1, result:len()-1)
+    end
+    return result.."}"
+end
+
 
 local _tostring = tostring
 
